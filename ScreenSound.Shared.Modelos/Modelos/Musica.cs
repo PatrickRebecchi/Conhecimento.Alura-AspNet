3e58b5b8c805e7337912e2b84cc895e3b8d649ac
Patrick <patrickrebecchi@hotmail.com>
2025-05-06 22:26:02 -0300

    Atualização

--- ScreenSound.Shared.Modelos/Modelos/Musica.cs
+++ ScreenSound.Shared.Modelos/Modelos/Musica.cs
@@ -1,8 +1,13 @@
-﻿
+﻿using ScreenSound.Shared.Modelos.Modelos;
+
 namespace ScreenSound.Modelos;
 
 public class Musica
 {
+    public Musica()
+    {
+
+    }
     public Musica(string nome)
     {
         Nome = nome;
@@ -11,16 +16,19 @@ public class Musica
     public string Nome { get; set; }
     public int Id { get; set; }
     public int? AnoLancamento { get; set; }
-
+    public int? ArtistaId { get; set; }
+    public virtual Artista? Artista { get; set; }
+    public virtual ICollection<Genero> Generos { get; set; }
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {Nome}");
-      
+
     }
 
     public override string ToString()
     {
         return @$"Id: {Id}
         Nome: {Nome}";
+
     }
 }
