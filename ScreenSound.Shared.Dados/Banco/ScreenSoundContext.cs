a873b795a8ba3670423334790c2a497d06e3de35
Patrick <patrickrebecchi@hotmail.com>
2025-05-06 22:23:35 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+public class ScreenSoundContext: DbContext
+{
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    //private string connectionString = "Data Source=screensoundserver01.database.windows.net,1433;User ID=patrick;Password=********;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
